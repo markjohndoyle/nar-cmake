669f7e268b5b0ee3440540ff044566d6bec133c1
Mark Doyle <markjohndoyle@googlemail.com>
2015-02-22 13:51:46 +0100

    Feature additions
     * Properties are gathered from the pom hierarchy
     * Version properties are used for dependencies.
     * Accepts project version arg
     * Added inc and src paths to build options
     * Removed some debug outs

--- main/generateCmake.py
+++ main/generateCmake.py
@@ -8,7 +8,7 @@ __author__ = 'Mark'
 def main():
     print("Parsing root pom  " + sys.argv[1])
     parser = pomParser.PomParser()
-    parser.parsePom(sys.argv[1])
+    parser.parsePom(sys.argv[1], sys.argv[2])
 
 
 if __name__ == '__main__':
--- main/pomParser.py
+++ main/pomParser.py
@@ -1,14 +1,11 @@
 import xml.etree.cElementTree as ETree
 from builtins import print
 import os
-import pprint
 
 from main import mavenDependency as mvnDep
 
 
 __author__ = 'Mark'
-
-# Parses nar module poms
 
 
 class PomParser:
@@ -18,40 +15,44 @@ class PomParser:
 
     dependencyVersions = {}
     dependencies = []
-    buildOptions = {"compiler": "g++",
-                    "linker": "g++",
-                    "debug": True,
-                    "compilerFlags": {"-std=c++11"},
-                    "sysLibs": set(),
-                    "output": "executable"}
+    buildOptions = {
+        "incPath": "src/main/include",
+        "srcPath": "src/main/c++",
+        "compiler": "g++",
+        "linker": "g++",
+        "debug": True,
+        "compilerFlags": {"-std=c++11"},
+        "sysLibs": set(),
+        "output": "executable"}
 
     properties = {}
 
 
     # Parses the pom pulling out nar specifics.
-    def parsePom(self, filepath):
+    def parsePom(self, filepath, projectVersion):
         print("Parsing pom " + filepath)
+        self.projectVersion = projectVersion
 
         tree = ETree.parse(filepath)
-        project = tree.getroot()
+        projectElem = tree.getroot()
 
-        parent = self.getParent(project)
+        parent = self.getParent(projectElem)
         if parent:
-            print("Has parent")
-            parentFile = self.findParentFile(parent, filepath)
+            parentFile = self.parseParentPom(parent, filepath)
 
-        plugins = project.findall("mvn:build/mvn:pluginManagement/mvn:plugins/mvn:plugin", self.ns)
+        self.gatherProperties(projectElem)
+
+        plugins = projectElem.findall("mvn:build/mvn:pluginManagement/mvn:plugins/mvn:plugin", self.ns)
         self.gatherNarPluginConfig(plugins)
-        plugins = project.findall("mvn:build/mvn:plugins/mvn:plugin", self.ns)
+        plugins = projectElem.findall("mvn:build/mvn:plugins/mvn:plugin", self.ns)
         self.gatherNarPluginConfig(plugins)
 
-        dependencyManagements = project.findall("mvn:dependencyManagement/mvn:dependencies/mvn:dependency", self.ns)
+        dependencyManagements = projectElem.findall("mvn:dependencyManagement/mvn:dependencies/mvn:dependency", self.ns)
         self.gatherAllNarDepManagement(dependencyManagements)
 
-        dependencies = project.findall("mvn:dependencies/mvn:dependency", self.ns)
+        dependencies = projectElem.findall("mvn:dependencies/mvn:dependency", self.ns)
         self.gatherDependencies(dependencies)
 
-        pprint.pprint(self.buildOptions, indent=2)
         for dep in self.dependencies:
             print(dep.getAol("gpp"))
 
@@ -72,19 +73,17 @@ class PomParser:
         parent = projectNode.find("mvn:parent", self.ns)
         return parent
 
-    def findParentFile(self, parentNode, filePath):
+    def parseParentPom(self, parentNode, filePath):
         relativePath = parentNode.find("mvn:relativePath", self.ns)
         if not relativePath:
             relativePath = self.parentPathDefault
-
         parentDir = os.path.dirname(os.path.dirname(filePath))
-        self.parsePom(parentDir + "/pom.xml")
+        self.parsePom(parentDir + "/pom.xml", self.projectVersion)
 
     def gatherDependencies(self, dependencies):
         for dependency in dependencies:
             typeDef = dependency.find("mvn:type", self.ns)
             if typeDef is not None:
-                print("Found nar dep")
                 groupId = dependency.find("mvn:groupId", self.ns).text
                 artifactId = dependency.find("mvn:artifactId", self.ns).text
                 type = typeDef.text
@@ -95,6 +94,9 @@ class PomParser:
                     managedVersion = self.dependencyVersions[groupId + "." + artifactId]
                     if managedVersion is not None:
                         version = managedVersion
+                    else:
+                        print("WARN - " + artifactId + "." + artifactId + " dependency has no version, ignoring")
+                        return
                 dep = mvnDep.MavenDependency(groupId, artifactId, version, type)
                 self.dependencies.append(dep)
 
@@ -107,7 +109,19 @@ class PomParser:
                     groupId = management.find("mvn:groupId", self.ns).text
                     artifactId = management.find("mvn:artifactId", self.ns).text
                     version = management.find("mvn:version", self.ns).text
+                    if version.startswith("${"):
+                        if version == "${project.version}":
+                            version = self.projectVersion
+                        else:
+                            version = self.properties[version]
                     self.dependencyVersions[groupId + "." + artifactId] = version
+
+    def gatherProperties(self, projectElem):
+        for prop in projectElem.findall("mvn:properties/*", self.ns):
+            mvnNamespace = "{" + self.ns["mvn"] + "}"
+            if prop.tag.startswith(mvnNamespace):
+                propKey = "${" + prop.tag.replace(mvnNamespace, "", 1) + "}"
+                self.properties[propKey] = prop.text;
 
 
 
