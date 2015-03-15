0701333f007a8e983be7b4e304a6dedff714ffaf
Mark Doyle <markjohndoyle@googlemail.com>
2015-03-15 12:02:06 +0100

    Added distutils based setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='narcmake',
+    version='1.0.0',
+    packages=['narcmake'],
+    url='https://github.com/markjohndoyle/nar-cmake',
+    license='',
+    author='Mark Doyle',
+    author_email='markjohndoyle@googlemail.com',
+    description='Generates Cmake files for nar based maven projects.'
+)
